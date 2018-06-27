a4e52f545eef7f928b6b3879f023a962c45b2bc7
Kevin Le <kevin.kent.le@gmail.com>
2018-06-26 23:06:54 -0400

    Too slow

--- kmers/kmers.go
+++ kmers/kmers.go
@@ -3,15 +3,22 @@ package kmers
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 type Kmers struct {
-	src   string
-	lines []string
+	src       string
+	lines     []string
+	Headers   []string // location of all the headers in lines.
+	Sequences []string // location of all the sequences in lines.
+	li        int      // line index in Headers and Sequences.
+	pi        int      // position index in a slice of Sequences.
+	K         int
 }
 
-func load(km *Kmers) {
+func (km *Kmers) load() {
 	file, err := os.Open(km.src)
 	if err != nil {
 		fmt.Println(err)
@@ -27,14 +34,64 @@ func load(km *Kmers) {
 	}
 }
 
+func (km *Kmers) index() {
+	seq := ""
+	for _, line := range km.lines {
+		if strings.HasPrefix(line, ">") {
+			if seq != "" {
+				km.Sequences = append(km.Sequences, seq)
+				seq = ""
+			}
+			km.Headers = append(km.Headers, line)
+		} else {
+			seq = seq + line
+		}
+	}
+	km.Sequences = append(km.Sequences, seq)
+	km.lines = nil
+}
+
 func New(s string) *Kmers {
 	km := &Kmers{
 		src: s,
+		li:  0,
+		pi:  0,
+		K:   11,
 	}
-	load(km)
+	km.load()
+	km.index()
 	return km
 }
 
 func (km *Kmers) Next() (string, string) {
-	return km.lines[0], km.lines[1]
+	lastOfSequences := km.li == len(km.Sequences)-1
+	endOfSeq := km.pi > len(km.Sequences[km.li])-1+km.K
+
+	// Done.
+	if lastOfSequences && endOfSeq {
+		return "", ""
+	}
+
+	// Move to next sequence.
+	if endOfSeq {
+		km.li++
+		km.pi = 0
+	}
+
+	// K is greater than the size of the contig.
+	if km.K > len(km.Sequences[km.li])-1 {
+		log.Printf("WARNING: contig %s is shorter than the chosen k-value of %v. Skipping contig.", km.Headers[km.li], km.K)
+		km.li++
+		km.pi = 0
+	}
+
+	// Fasta header.
+	header := km.Headers[km.li]
+	// Slice of the sequence.
+	sl := km.Sequences[km.li][km.pi : km.pi+km.K]
+
+	// Increment.
+	km.pi += km.K
+
+	return header, sl
 }
