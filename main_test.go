8f14a3213921916e585e4720bed7371920e76402
Kevin Le <kevin.kent.le@gmail.com>
2018-08-05 23:12:58 -0400

    Change: use a shortened genome file for testing CreateAll()

--- main_test.go
+++ main_test.go
@@ -182,7 +182,7 @@ func BenchmarkCreateAll(b *testing.B) {
 	g := pangenome.NewGraph()
 	log.Println("Graph created OK.")
 	defer g.Close()
-	km := kmers.New("testdata/GCA_900015695.1_ED647_contigs_genomic.fna")
+	km := kmers.New("testdata/GCA_900015695.1_ED647_contigs_genomic_SHORTENED.fna")
 	log.Println("Kmers created OK.")
 	b.ResetTimer()
 	log.Println("Starting Node/Edge creation.")
@@ -196,7 +196,7 @@ func ExampleCreateAll() {
 
 	g := pangenome.NewGraph()
 	defer g.Close()
-	km := kmers.New("testdata/GCA_900015695.1_ED647_contigs_genomic.fna")
+	km := kmers.New("testdata/GCA_900015695.1_ED647_contigs_genomic_SHORTENED.fna")
 	b, _ := g.CreateAll(km, contextMain)
 	fmt.Println(b)
 	// Output:
